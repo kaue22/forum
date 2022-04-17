44fe63ee55ec0bc6b4c4a48a3c12bf6ea6149cf4
kaue <kauegouveia@gmail.com>
2022-04-17 10:49:02 -0300

    Criando rota post no projeto

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,20 +1,31 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
     private TopicoRepository topicoRepository;
-    @RequestMapping("/topicos")
+    
+
+    @Autowired
+    private CursoRepository cursoRepository;
+   //@RequestMapping(value = "/topicos", method = RequestMethod.GET)
+    @GetMapping
     public List<TopicoDto> lista(String nomeCurso){
     	if(nomeCurso == null) {
         List<Topico> topics = topicoRepository.findAll();
@@ -23,6 +34,13 @@ public class TopicosController {
     	List<Topico> topics = topicoRepository.findByCursoNome(nomeCurso);
     	return TopicoDto.converter(topics);
     	}
-       
+    }
+    
+   //@RequestMapping(value = "/topicos", method = RequestMethod.POST)
+    @PostMapping
+    public void cadastrar(@RequestBody TopicoForm form) {
+    	Topico topico = form.converter(cursoRepository);
+    	topicoRepository.save(topico);
+    	
     }
 }
---
+++ src/main/java/br/com/alura/forum/repository/CursoRepository.java
@@ -0,0 +1,16 @@
+package br.com.alura.forum.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+
+public interface CursoRepository extends JpaRepository<Curso, Long> {
+
+	List<Topico> findByCursoNome(String nomeCurso);
+
+	Curso findByNome(String nomeCurso);
+
+}
